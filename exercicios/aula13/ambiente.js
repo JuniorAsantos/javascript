f29b385f22d2b069c42da790a4bc4612f316cb4f
JuniorAsantos <juniorasantos54@gmail.com>
2021-11-22 21:08:57 -0300

    Atualizado exercicio

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 10)
+
+/*
+var c = 1
+while(c <= 6){
+    console.log(`Passo ${c}`);
+    c++
+}
+*/
+/*
+console.log('Tudo Bem!');
+console.log('Tudo Bem!');
+console.log('Tudo Bem!');
+console.log('Tudo Bem!');
+console.log('Tudo Bem!');
+console.log('Tudo Bem!');
+*/
